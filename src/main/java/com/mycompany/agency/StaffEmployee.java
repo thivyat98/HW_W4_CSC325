8ce3148967c7f10e3df78a4dfde7c574bd1361e2
Mahin Khandker <56740284+mkhandker19@users.noreply.github.com>
2024-09-21 22:11:55 -0400

    Completed StaffEmployee class

--- src/main/java/com/mycompany/agency/StaffEmployee.java
+++ src/main/java/com/mycompany/agency/StaffEmployee.java
@@ -3,7 +3,7 @@ package com.mycompany.agency;
 
     // TODO 1: Make StaffEmployee a child of StaffMember
 
-public class StaffEmployee 
+public class StaffEmployee extends StaffMember
 {
     protected String socialSecurityNumber;
     protected double payRate;
@@ -27,7 +27,7 @@ public class StaffEmployee
     //-----------------------------------------------------------------
     public String toString()
     {
-    
+    return super.toString() + "\nSocial Security Number: " + socialSecurityNumber + "\nPay Rate: " + payRate;
     }
 
     //-----------------------------------------------------------------
@@ -35,6 +35,6 @@ public class StaffEmployee
     //-----------------------------------------------------------------
     public double pay()
     {
-       
+       return payRate;
     }
 }
