566942b7bda2e51e63c3e3677b2a4750d525454f
thivyat98 <thust@farmingdale.edu>
2024-09-21 22:42:39 -0400

    Added intern class as subclass of StaffEmployee Implemented constructor and pay method

--- src/main/java/com/mycompany/agency/Intern.java
+++ src/main/java/com/mycompany/agency/Intern.java
@@ -2,24 +2,13 @@ package com.mycompany.agency;
 
 
 
-    // TODO 1: Make Intern a child of StaffEmploee
-
-public class Intern 
-{
-    //-----------------------------------------------------------------
-    //  TODO 2: Constructor: Sets up this intern using the specified
-    //  information.
-    //-----------------------------------------------------------------
-    public Intern(String eName, String eAddress, String ePhone)
-    {
-        
-    }
-
-    //-----------------------------------------------------------------
-    // TODO 3:  Returns a zero pay value for this intern.
-    //-----------------------------------------------------------------
-    public double pay()
-    {
+   public class Intern extends StaffEmployee{
+       
+public Intern(String eName,String eAddress,String ePhone) {
+        super(eName, eAddress, ePhone);
+   }
+    public double pay() {
+       return 0.0;
       
     }
 }
