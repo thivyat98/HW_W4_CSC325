69279475638cf4faffed88a48f6926b51f703797
selvap1 <selvap@farmingdale.edu>
2024-09-22 22:03:23 -0400

    Updated Intern.java
    Made Intern a child of StaffEmployee
    Set up constructor
    Returned zero pay value for this intern

--- src/main/java/com/mycompany/agency/Intern.java
+++ src/main/java/com/mycompany/agency/Intern.java
@@ -1,25 +1,22 @@
 package com.mycompany.agency;
 
+// TODO 1: Make Intern a child of StaffEmployee
+public class Intern extends StaffEmployee {
 
-
-    // TODO 1: Make Intern a child of StaffEmploee
-
-public class Intern 
-{
     //-----------------------------------------------------------------
-    //  TODO 2: Constructor: Sets up this intern using the specified
-    //  information.
+    //  Constructor: Sets up this intern using the specified information.
     //-----------------------------------------------------------------
-    public Intern(String eName, String eAddress, String ePhone)
-    {
-        
+    public Intern(String eName, String eAddress, String ePhone) {
+        // Since interns don't have a social security number or pay rate, we pass default values
+        super(eName, eAddress, ePhone, "N/A", 0.0); // social security number as "N/A" and pay rate as 0.0
     }
 
     //-----------------------------------------------------------------
     // TODO 3:  Returns a zero pay value for this intern.
     //-----------------------------------------------------------------
-    public double pay()
-    {
-      
+    @Override
+    public double pay() {
+        // Interns have no pay, so always return 0.0
+        return 0.0;
     }
 }
